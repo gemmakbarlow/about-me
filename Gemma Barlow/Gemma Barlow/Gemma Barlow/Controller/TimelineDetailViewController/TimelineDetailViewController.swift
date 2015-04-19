00768f8aa35911bc331f7e959bfa0867dfa471b7
Gemma Barlow <gemmakbarlow@gmail.com>
2015-04-18 22:20:52 -0400

    Minor changes - including text changes.

---
+++ Gemma Barlow/Gemma Barlow/Gemma Barlow/Controller/TimelineDetailViewController/TimelineDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TimelineDetailViewController.swift
+//  Gemma Barlow
+//
+//  Created by Gemma Barlow on 4/18/15.
+//
+//
+
+import UIKit
+
+class TimelineDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.backgroundGrayColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
