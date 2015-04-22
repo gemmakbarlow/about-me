088118488d39cc097b44fe46ad6ddef5f6e7b86c
Gemma Barlow <gemmakbarlow@gmail.com>
2015-04-21 22:44:20 -0400

    Added Basic Launch Screen

--- Gemma Barlow/Gemma Barlow/Gemma Barlow/AppDelegate.swift
+++ Gemma Barlow/Gemma Barlow/Gemma Barlow/AppDelegate.swift
@@ -16,11 +16,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return true
     }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-
-    }
-
-
 }
 
